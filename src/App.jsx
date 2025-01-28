8893c905e735f5b99aaec34e50e53f74a29d710f
Aaridhi <aaridhi0301@gmail.com>
2025-01-29 00:35:28 +0530

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import {Route, Routes} from "react-router-dom"
+import OverviewPage from "./pages/OverviewPage"
+import ProductsPage from "./pages/ProductsPage"
 
-function App({name}) {
-  console.log(name);
-  return <div className='text-red-500 text-4xl bg-blue-500'>Hello
-  It's a beautiful day
-  </div>
+import Sidebar from "./components/Sidebar";
+function App() {
+  
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/*BG */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80">
+          <div className="absolute inset-0 backdrop-blur-sm"></div>
+
+        </div>
+      </div>
+      <Sidebar></Sidebar>
+      <Routes>
+        <Route path='/' element={<OverviewPage />} />
+        <Route path='/products' element={<ProductsPage />} />
+      </Routes>
+    </div>
+  )
 
 }
 
